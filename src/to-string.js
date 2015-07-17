516848d9dcf24eed91b2aefdd8346b85b5e6b509
Gajus Kuizinas <gk@anuary.com>
2015-07-17 13:16:14 +0100

    Reference to the docs.

--- src/to-string.js
+++ src/to-string.js
@@ -1,3 +1,6 @@
-module.exports = function(content) {
-	return 'module.exports = ' + JSON.stringify(this.exec(content, this.resource).toString());
+/**
+ * @see https://github.com/webpack/webpack/wiki/Loader-Specification
+ */
+module.exports = function (content) {
+    return 'module.exports = ' + JSON.stringify(this.exec(content, this.resource).toString());
 }
