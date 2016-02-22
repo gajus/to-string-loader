dffa33c02de044265565e77eaca625f565114b23
Alexander Blazheev <dontrelaxman@gmail.com>
2016-02-22 13:17:57 +0000

    Flag loader as cachable

--- src/to-string.js
+++ src/to-string.js
@@ -2,5 +2,6 @@
  * @see https://github.com/webpack/webpack/wiki/Loader-Specification
  */
 module.exports = function (content) {
+    this.cacheable();
     return 'module.exports = ' + JSON.stringify(this.exec(content, this.resource).toString());
 };
