c95e13f14b46e03795e8b77b0814ce5b45647a49
Adam Lee <duo1123@gmail.com>
2017-02-22 01:38:12 -0500

    feat(sourcemaps): add support for pass through of sourcemaps
    works with css-loader

--- src/to-string.js
+++ src/to-string.js
@@ -12,12 +12,17 @@ module.exports.pitch = function(remainingRequest) {
     if (this.cacheable) {
         this.cacheable();
     }
+    const query = loaderUtils.parseQuery(this.query);
 
     return `
         var result = require(${loaderUtils.stringifyRequest(this, "!!" + remainingRequest)});
 
         if (typeof result === "string") {
             module.exports = result;
+        } else if (${query.sourceMap}) {
+            module.exports = \`\${result[0][1]}
+
+\${"/\\\*\#"} sourceMappingURL=data:application/json;base64,\${btoa(JSON.stringify(result[0][3]))} */\`;
         } else {
             module.exports = result.toString();
         }
